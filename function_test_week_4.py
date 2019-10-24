66c1d7e49a298d77a7a49dd342b53b5b0efc2827
theoehrmann <te319@ic.ac.uk>
2019-10-24 13:52:23 +0100

    1

---
+++ function_test_week_4.py
@@ -0,0 +1,7 @@
+
+
+def first(input_value) :
+    """returns with plus 3"""
+    
+    input_value = '3' + input_value
+    return input_value
--- functiontesting.py
+++ functiontesting.py
@@ -1,9 +1,9 @@
-def f1(y)
+def f1(y) :
     A_variable == y + 1
     print(A_variable)
 
 
-def f2(x)
+def f2(x) :
     B_variable == x + 1
     return(B_variable)
 
